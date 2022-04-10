eb6faafeb37239a3c8bb4b995d8c14ab598e06a8
kpk <k.prabhatkumar.05@gmail.com>
2022-04-10 20:11:20 +0530

    back

--- hungry.py
+++ hungry.py
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-print("I am hungry and i want to have something to eat. Please give me some food")
-print("hi")
-=======
 print("I am hungry")
->>>>>>> parent of 5490bfe (added more to print)
